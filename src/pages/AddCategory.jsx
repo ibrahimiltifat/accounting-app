eff312dfd625dc9022106e7aae9e3ccff1b63a39
mi014378 <38471029+mi014378@users.noreply.github.com>
2022-02-26 00:55:01 +0500

    first commit

---
+++ src/pages/AddCategory.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function AddCategory() {
+  const [categories, setCategories] = useState([]);
+  const [message, SetMessage] = useState("");
+  const [newCategory, SetNewCategory] = useState("");
+
+  const handleTextChange = (e) => {
+    SetNewCategory(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newCategory.length === 0) {
+      SetMessage("Please enter a valid Category");
+    } else {
+      if (categories.includes(newCategory)) {
+        SetMessage("Category already present");
+      } else {
+        SetMessage("");
+        setCategories([newCategory, ...categories]);
+      }
+      console.log(categories);
+      SetNewCategory("");
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="card">
+        <form onSubmit={handleSubmit}>
+          <h2>ENTER A NEW CATEGORY</h2>
+          <div className="input-group">
+            <input
+              onChange={handleTextChange}
+              type="text"
+              placeholder="Enter a Category"
+              value={newCategory}
+            />
+          </div>
+          <button className="btn btn-primary" type="submit">
+            submit
+          </button>
+          {message && <div className="message">{message}</div>}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default AddCategory;
